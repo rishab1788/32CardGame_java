7b29cb4219beca09f9dc294a2758deb9a9606763
Rishabh1788 <rishab178@gmail.com>
2019-10-18 15:35:36 +0530

    adding function of player like drawncard and pilecards method

--- 32CardsGame_javaAssignment/src/Players.java
+++ 32CardsGame_javaAssignment/src/Players.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Players {
 
@@ -7,15 +8,37 @@ public class Players {
     public void playerWork(Pile p[], int playerNumber) throws InterruptedException {
         synchronized (this) {
             printPlayerCard(playerNumber);
-
-            withDrawCardFromPile(playerNumber);
-
+            Card c = withDrawCardFromPile(p, playerNumber);
+            addwithCardToPlayer(c);
+            playerCardToWithDraw();
+            addCardToPile(p, playerNumber);
         }
     }
 
-    private void withDrawCardFromPile(int playerNumber) {
+    private void playerCardToWithDraw() {
+        HashMap<Card, Integer> hm = new HashMap();
+        for (int i = 0; i < cards.size(); i++) {
+            if (hm.containsKey(cards.get(i))) {
+                hm.put(cards.get(i), hm.get(cards.get(i)) + 1);
+            } else {
+                hm.put(cards.get(i), 0);
+            }
+        }
+
+    }
+
+    private void addwithCardToPlayer(Card c) {
+        cards.add(c);
+    }
+
+    private void addCardToPile(Pile[] p, int playerNumber) {
+        //p[(playerNumber + 1) % 4].cards.add();
+    }
+
+    private Card withDrawCardFromPile(Pile p[], int playerNumber) {
         int pileToWithDraw = playerNumber;
 
+        return p[playerNumber].cards.remove(0);//top
     }
 
     private void printPlayerCard(int playerNumber) throws InterruptedException {
