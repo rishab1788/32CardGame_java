e812384de2c7ca1c8c204aaffe28b90858a954dd
Rishabh1788 <rishab178@gmail.com>
2019-10-24 16:43:19 +0530

    players implemented run and functionalatiy switch

--- 32CardsGame_javaAssignment/src/Main.java
+++ 32CardsGame_javaAssignment/src/Main.java
@@ -1,74 +1,70 @@
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.CountDownLatch;
+
 import static java.lang.Thread.sleep;
 
 public class Main implements Runnable {
-    Players players[] = new Players[4];
-    Pile piles[] = new Pile[4];
+
     Deck d = new Deck();
 
-    Main() { //instansication
-        for (int i = 0; i < 4; i++) {
-            piles[i] = new Pile();
-        }
-        //instansication
 
-        for (int i = 0; i < 4; i++) {
-            players[i] = new Players();
-        }
-        distributeCardsPlayersPile();
+    Main() { //instansication
+
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         Main hand = new Main();
-        Thread t1 = new Thread(hand);
+        Players player = new Players();
+        System.out.print(player);
+        hand.distributeCardsPlayersPile(player);
+        CountDownLatch startSignal = new CountDownLatch(1);
+        Thread t1 = new Thread(player);
         t1.setName("1");
-        Thread t2 = new Thread(hand);
+        Thread t2 = new Thread(player);
         t2.setName("2");
-        Thread t3 = new Thread(hand);
+        Thread t3 = new Thread(player);
         t3.setName("3");
-        Thread t4 = new Thread(hand);
+        Thread t4 = new Thread(player);
         t4.setName("4");
-
         t1.start();
-        sleep(500);
+        t1.join();
         t2.start();
-        sleep(500);
+        t2.join();
         t3.start();
-        sleep(500);
+        t3.join();
         t4.start();
-
     }
 
     @Override
     public void run() {
 
-        int playerNumber = Integer.parseInt(Thread.currentThread().getName());
+     /*   int playerNumber = Integer.parseInt(Thread.currentThread().getName());
 
         try {
             players[playerNumber - 1].playerWork(piles, playerNumber - 1);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
+*/
 
     }
 
-    public void distributeCardsPlayersPile() {
+    public void distributeCardsPlayersPile(Players players) {
         int temp = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                players[i].cards.add(d.card[temp]);
+                players.cardsListOfPlayers[i].add(d.card[temp]);
                 System.out.println("player" + i + " = " + d.card[temp].getCardValue());
                 temp++;
             }
             for (int j = 4; j < 8; j++) {
-                piles[i].cards.add(d.card[temp]);
+                players.piles[i].cards.add(d.card[temp]);
                 System.out.println("pile" + i + " = " + d.card[temp].getCardValue());
                 temp++;
             }
         }
 
     }
-
-
 }
--- 32CardsGame_javaAssignment/src/Players.java
+++ 32CardsGame_javaAssignment/src/Players.java
@@ -1,16 +1,144 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 import static java.lang.Thread.sleep;
+import static java.lang.Thread.yield;
 
-public class Players {
+public class Players implements Runnable {
     static int winner = -1;
     static int time = 10;
-    ArrayList<Card> cards = new <Card>ArrayList(4);
+    static int turn = 0;
+    ArrayList<Card> cardsListOfPlayers[] = new ArrayList[4];
+    Pile piles[] = new Pile[4];
 
-    public synchronized void playerWork(Pile p[], int playerNumber) throws InterruptedException {
+    Players() {
+        for (int i = 0; i < 4; i++) {
+            cardsListOfPlayers[i] = new ArrayList(4);
+        }
+        for (int i = 0; i < 4; i++) {
+            piles[i] = new Pile();
+        }
+
+    }
+
+    @Override
+    public synchronized void run() {
+        while (winner == -1) {
+            while (turn!=)
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            System.out.println(Thread.currentThread().getName());
+            int playerNumber = Integer.parseInt(Thread.currentThread().getName());
+            playerNumber = playerNumber - 1;
+            printPlayerCard(playerNumber);
+            Card withDrawnCardFromPile = withDrawCardFromPile(playerNumber);
+            addPileCardToPlayer(playerNumber, withDrawnCardFromPile);
+            printPlayerCard(playerNumber);
+            if (isPlayerWon(playerNumber) == true) {
+                System.out.println("player " + playerNumber + "Won The Match");
+                winner = playerNumber;
+                return;
+            }
+            Card withDrawnCardFromPlayer = playerCardToWithDraw(playerNumber);
+            addCardToNextPile(playerNumber, withDrawnCardFromPlayer);
+            printPlayerCard(playerNumber);
+            try {
+                wait(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        if (winner != -1) {
+            System.out.println("Player " + "GoodBye Congrats To Player-" + winner);
+        }
+    }
+
+    private void addCardToNextPile(int playerNumber, Card card) {
+        piles[(playerNumber + 1) % 4].cards.add(card);
+    }
+
+    private Card withDrawCardFromPile(int playerNumber) {
+        int pileToWithDraw = playerNumber;
+        return piles[playerNumber].cards.remove(0);//top
+    }
+
+    private synchronized void printPlayerCard(int playerNumber) {
+
+        System.out.print("Player number " + playerNumber + " cards-");
+        for (int i = 0; i < cardsListOfPlayers[playerNumber].size(); i++) {
+            System.out.print(cardsListOfPlayers[playerNumber].get(i).cardValue + ",");
+        }
+        System.out.println();
+    }
+
+    private void addPileCardToPlayer(int playerNumber, Card c) {
+        cardsListOfPlayers[playerNumber].add(c);
+    }
+
+    private boolean isPlayerWon(int playerNumber) {
+        HashMap<Card, Integer> hm = new HashMap();
+        for (int i = 0; i < cardsListOfPlayers[playerNumber].size(); i++) {
+            if (hm.containsKey(cardsListOfPlayers[playerNumber].get(i))) {
+                hm.put(cardsListOfPlayers[playerNumber].get(i), hm.get(cardsListOfPlayers[playerNumber].get(i)) + 1);
+            } else {
+                hm.put(cardsListOfPlayers[playerNumber].get(i), 1);
+            }
+        }
+        int maxOccurance = 0;
+        int maxOccuranceValue = 0;
+        for (Map.Entry<Card, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() > maxOccurance) {
+                maxOccurance = entry.getValue();
+                maxOccuranceValue = entry.getKey().cardValue;
+
+            }
+
+        }
+
+        System.out.println("Max Occurance-" + maxOccurance);
+        if (maxOccurance == 4) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private Card playerCardToWithDraw(int playerNumber) {
+
+        HashMap<Card, Integer> hm = new HashMap();
+        for (int i = 0; i < cardsListOfPlayers[playerNumber].size(); i++) {
+            if (hm.containsKey(cardsListOfPlayers[playerNumber].get(i))) {
+                hm.put(cardsListOfPlayers[playerNumber].get(i), hm.get(cardsListOfPlayers[playerNumber].get(i)) + 1);
+            } else {
+                hm.put(cardsListOfPlayers[playerNumber].get(i), 1);
+            }
+        }
+        int minOccurance = 10;
+        int minOccuranceValue = 0;
+        Random rand = new Random();
+        int tempvaiable = rand.nextInt(5);
+        for (Map.Entry<Card, Integer> entry : hm.entrySet()) {
+            if (entry.getValue() <= minOccurance) {
+                minOccurance = entry.getValue();
+                minOccuranceValue = entry.getKey().cardValue;
+                tempvaiable--;
+                if (tempvaiable == 0 && minOccurance == 1) {
+                    break;
+                }
+            }
+        }
+
+        cardsListOfPlayers[playerNumber].remove(new Card(minOccuranceValue));
+        System.out.println("CardsWithDrawn-" + minOccuranceValue);
+        return new Card(minOccuranceValue);
+    }
+    /*public synchronized void playerWork(Pile p[], int playerNumber) throws InterruptedException {
         while (winner == -1) {
             System.out.println("-----------------------------------------");
             printPlayerCard(playerNumber);
@@ -25,24 +153,26 @@ public class Players {
             Card withDrawCard = playerCardToWithDraw();
             printPlayerCard(playerNumber);
             addCardToPile(p, playerNumber, withDrawCard);
-            Thread.sleep(time+1000);
-            wait(2000  );
+            Thread.sleep(time + 1000);
+            wait(2000);
             time = time + 200;
         }
-
         if (winner != -1) {
             System.out.println("Player " + playerNumber + "GoodBye Congrats To Player-" + winner);
         }
     }
 
+*/
+/*
+
 
     private boolean isPlayerWin() throws InterruptedException {
         HashMap<Card, Integer> hm = new HashMap();
-        for (int i = 0; i < cards.size(); i++) {
-            if (hm.containsKey(cards.get(i))) {
-                hm.put(cards.get(i), hm.get(cards.get(i)) + 1);
+        for (int i = 0; i < cardsListOfPlayers.size(); i++) {
+            if (hm.containsKey(cardsListOfPlayers.get(i))) {
+                hm.put(cardsListOfPlayers.get(i), hm.get(cardsListOfPlayers.get(i)) + 1);
             } else {
-                hm.put(cards.get(i), 1);
+                hm.put(cardsListOfPlayers.get(i), 1);
             }
         }
         int maxOccurance = 0;
@@ -68,11 +198,11 @@ public class Players {
     private Card playerCardToWithDraw() {
 
         HashMap<Card, Integer> hm = new HashMap();
-        for (int i = 0; i < cards.size(); i++) {
-            if (hm.containsKey(cards.get(i))) {
-                hm.put(cards.get(i), hm.get(cards.get(i)) + 1);
+        for (int i = 0; i < cardsListOfPlayers.size(); i++) {
+            if (hm.containsKey(cardsListOfPlayers.get(i))) {
+                hm.put(cardsListOfPlayers.get(i), hm.get(cardsListOfPlayers.get(i)) + 1);
             } else {
-                hm.put(cards.get(i), 1);
+                hm.put(cardsListOfPlayers.get(i), 1);
             }
         }
         int minOccurance = 10;
@@ -85,7 +215,7 @@ public class Players {
             }
 
         }
-        cards.remove(new Card(minOccuranceValue));
+        cardsListOfPlayers.remove(new Card(minOccuranceValue));
         System.out.println("CardsWithDrawn-" + minOccuranceValue);
         return new Card(minOccuranceValue);
     }
@@ -93,23 +223,7 @@ public class Players {
     private void addPileCardToPlayer(Card c) {
         cards.add(c);
     }
-
-    private void addCardToPile(Pile[] p, int playerNumber, Card card) {
-        p[(playerNumber + 1) % 4].cards.add(card);
-    }
-
-    private Card withDrawCardFromPile(Pile p[], int playerNumber) {
-        int pileToWithDraw = playerNumber;
-        return p[playerNumber].cards.remove(0);//top
-    }
-
-    private void printPlayerCard(int playerNumber) throws InterruptedException {
-        System.out.print("Player number " + playerNumber + " cards-");
-        for (int i = 0; i < cards.size(); i++) {
-            System.out.print(cards.get(i).cardValue + ",");
-        }
-        System.out.println();
+*/
 
 
-    }
 }
